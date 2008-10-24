6061a41e2ffe1cc62e212147c7701926884a120d
gehannf <gehannf@928d84fa-9f5f-11dd-adfd-a1ec4d499d49>
2008-10-24 22:52:38 +0000

    C# rewrite! + implemented user specific dateformat + implemented user specific splittrigger (time between 2 lines) # changed codebase from c++ cli to C# # changed code comments to english from german for better understanding, at least I hope so! ~ delete present splittet logfiles before creating them again. Before the Program would try to append them.

---
+++ trunk/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿/*
+    WoW Combatlog Splitter
+    Copyright (C) 2008 Frank Gehann (Takoura EU-Perenolde)
+	
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace WoW_Combatlog_Splitter
+{
+    partial class WoWCombatlogSplitter
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WoWCombatlogSplitter));
+            this.btn_combatlog = new System.Windows.Forms.Button();
+            this.lbl_combatlog = new System.Windows.Forms.Label();
+            this.dlg_combatlog = new System.Windows.Forms.OpenFileDialog();
+            this.lbl_filename = new System.Windows.Forms.Label();
+            this.prg_parse = new System.Windows.Forms.ProgressBar();
+            this.btn_parse = new System.Windows.Forms.Button();
+            this.btn_abort = new System.Windows.Forms.Button();
+            this.lbl_link = new System.Windows.Forms.LinkLabel();
+            this.edt_dateformat = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edt_splittime = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_combatlog
+            // 
+            this.btn_combatlog.Location = new System.Drawing.Point(12, 12);
+            this.btn_combatlog.Name = "btn_combatlog";
+            this.btn_combatlog.Size = new System.Drawing.Size(119, 23);
+            this.btn_combatlog.TabIndex = 0;
+            this.btn_combatlog.Text = "Load CombatLog";
+            this.btn_combatlog.UseVisualStyleBackColor = true;
+            this.btn_combatlog.Click += new System.EventHandler(this.btn_combatlog_Click);
+            // 
+            // lbl_combatlog
+            // 
+            this.lbl_combatlog.Location = new System.Drawing.Point(137, 12);
+            this.lbl_combatlog.Name = "lbl_combatlog";
+            this.lbl_combatlog.Size = new System.Drawing.Size(298, 23);
+            this.lbl_combatlog.TabIndex = 1;
+            this.lbl_combatlog.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dlg_combatlog
+            // 
+            this.dlg_combatlog.FileName = "WoWCombatLog.txt";
+            this.dlg_combatlog.Filter = "Text Dateien|*.txt|Alle Dateien|*.*";
+            this.dlg_combatlog.FileOk += new System.ComponentModel.CancelEventHandler(this.dlg_combatlog_FileOk);
+            // 
+            // lbl_filename
+            // 
+            this.lbl_filename.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbl_filename.Location = new System.Drawing.Point(143, -93);
+            this.lbl_filename.Name = "lbl_filename";
+            this.lbl_filename.Size = new System.Drawing.Size(252, 23);
+            this.lbl_filename.TabIndex = 2;
+            this.lbl_filename.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // prg_parse
+            // 
+            this.prg_parse.Location = new System.Drawing.Point(12, 72);
+            this.prg_parse.Name = "prg_parse";
+            this.prg_parse.Size = new System.Drawing.Size(423, 23);
+            this.prg_parse.Step = 1;
+            this.prg_parse.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.prg_parse.TabIndex = 3;
+            this.prg_parse.Visible = false;
+            // 
+            // btn_parse
+            // 
+            this.btn_parse.Enabled = false;
+            this.btn_parse.Location = new System.Drawing.Point(12, 42);
+            this.btn_parse.Name = "btn_parse";
+            this.btn_parse.Size = new System.Drawing.Size(119, 23);
+            this.btn_parse.TabIndex = 4;
+            this.btn_parse.Text = "Parse";
+            this.btn_parse.UseVisualStyleBackColor = true;
+            this.btn_parse.Visible = false;
+            this.btn_parse.Click += new System.EventHandler(this.btn_parse_Click);
+            // 
+            // btn_abort
+            // 
+            this.btn_abort.Location = new System.Drawing.Point(13, 43);
+            this.btn_abort.Name = "btn_abort";
+            this.btn_abort.Size = new System.Drawing.Size(118, 23);
+            this.btn_abort.TabIndex = 5;
+            this.btn_abort.Text = "Cancel";
+            this.btn_abort.UseVisualStyleBackColor = true;
+            this.btn_abort.Visible = false;
+            this.btn_abort.Click += new System.EventHandler(this.btn_abort_Click);
+            // 
+            // lbl_link
+            // 
+            this.lbl_link.AutoSize = true;
+            this.lbl_link.Location = new System.Drawing.Point(143, 98);
+            this.lbl_link.Name = "lbl_link";
+            this.lbl_link.Size = new System.Drawing.Size(161, 13);
+            this.lbl_link.TabIndex = 7;
+            this.lbl_link.TabStop = true;
+            this.lbl_link.Text = "http://wowclsp.googlecode.com";
+            this.lbl_link.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lbl_link_LinkClicked);
+            // 
+            // edt_dateformat
+            // 
+            this.edt_dateformat.Location = new System.Drawing.Point(183, 46);
+            this.edt_dateformat.Name = "edt_dateformat";
+            this.edt_dateformat.Size = new System.Drawing.Size(113, 20);
+            this.edt_dateformat.TabIndex = 8;
+            this.edt_dateformat.Text = "yyyy.mm.dd_HHss";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Format:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Split time (minutes):";
+            // 
+            // edt_splittime
+            // 
+            this.edt_splittime.Location = new System.Drawing.Point(405, 45);
+            this.edt_splittime.MaxLength = 5;
+            this.edt_splittime.Name = "edt_splittime";
+            this.edt_splittime.Size = new System.Drawing.Size(30, 20);
+            this.edt_splittime.TabIndex = 11;
+            this.edt_splittime.Text = "120";
+            // 
+            // WoWCombatlogSplitter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 116);
+            this.Controls.Add(this.edt_splittime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.edt_dateformat);
+            this.Controls.Add(this.lbl_link);
+            this.Controls.Add(this.btn_abort);
+            this.Controls.Add(this.btn_parse);
+            this.Controls.Add(this.prg_parse);
+            this.Controls.Add(this.lbl_filename);
+            this.Controls.Add(this.lbl_combatlog);
+            this.Controls.Add(this.btn_combatlog);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WoWCombatlogSplitter";
+            this.Text = "WoW Combatlog Splitter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_combatlog;
+        private System.Windows.Forms.Label lbl_combatlog;
+        private System.Windows.Forms.OpenFileDialog dlg_combatlog;
+        private System.Windows.Forms.Label lbl_filename;
+        private System.Windows.Forms.ProgressBar prg_parse;
+        private System.Windows.Forms.Button btn_parse;
+        private System.Windows.Forms.Button btn_abort;
+        private System.Windows.Forms.LinkLabel lbl_link;
+        private System.Windows.Forms.TextBox edt_dateformat;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox edt_splittime;
+    }
+}
+
---
+++ trunk/Form1.cs
@@ -0,0 +1,267 @@
+﻿/*
+    WoW Combatlog Splitter
+    Copyright (C) 2008 Frank Gehann (Takoura EU-Perenolde)
+	
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Net;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+
+namespace WoW_Combatlog_Splitter
+{
+    public partial class WoWCombatlogSplitter : Form
+    {
+        bool abort = false;
+
+        public WoWCombatlogSplitter()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_combatlog_Click(object sender, EventArgs e)
+        {
+            dlg_combatlog.ShowDialog();
+        }
+
+        private void btn_parse_Click(object sender, EventArgs e)
+        {
+            // Check if Dateformat is Valid!
+            Regex date_format = new Regex("^[MdyHms_.\\s-]*$", RegexOptions.Singleline);
+            Match md = date_format.Match(edt_dateformat.Text);
+
+            // Check if Splittime is Valid!
+            Regex split_time_format = new Regex("^[\\d]{1,5}$", RegexOptions.Singleline);
+            Match st = split_time_format.Match(edt_splittime.Text);
+
+            if (md.Success && st.Success)
+            { // everything seems to be ok so faar we'll see =)
+
+                btn_parse.Visible = false;
+                btn_parse.Enabled = false;
+                btn_abort.Visible = true;
+                btn_abort.Enabled = true;
+                btn_combatlog.Enabled = false;
+
+                // create variables
+                string file = dlg_combatlog.FileName;
+                string path = Path.GetDirectoryName(file);
+                string line = string.Empty;
+                string writelog = string.Empty;
+                string newlogname = string.Empty;
+                string oldlogname = string.Empty;
+                string logname = string.Empty;
+
+                // Regex for matching a date!
+                // Format: 6/8 14:58:33.943
+                // or
+                // Format: 10/18 14:58:33.943
+                Regex date_match = new Regex("^(?<months>[0-9]{1,2})/(?<days>[0-9]{1,2}) (?<hours>[0-9]{2}):(?<minutes>[0-9]{2}):(?<seconds>[0-9]{2}).(?<milliseconds>[0-9]{3})  .*$");
+
+                DateTime lastdate = new DateTime(2000, 1, 1); // set fake lastdate since we need some match for the first line!
+                DateTime currdate = DateTime.Now; // set current date for a match at the first line!
+                TimeSpan timediff;
+
+                long line_count = 0;
+                int diff_logs = 0;
+
+                Stopwatch parsetime = new Stopwatch();
+
+                // create the filestream objects
+                FileStream fs_olog = new FileStream(file, FileMode.Open, FileAccess.Read);
+                FileStream fs_nlog = null;
+                // create the streamreader objects
+                StreamReader sr_olog = new StreamReader(fs_olog);
+                StreamWriter sw_nlog = null;
+
+                // max progressbar length and make it visible!
+                prg_parse.Maximum = (int)fs_olog.Length;
+                prg_parse.Visible = true;
+
+                // start the stopwatch for the logcreating process
+                parsetime.Start();
+
+                while (!abort && (line = sr_olog.ReadLine()) != null)
+                {
+                    // get the current linedate by matching via regular expressions
+                    Match line_match = date_match.Match(line);
+                    GroupCollection group_match = line_match.Groups;
+
+                    // set the parsed linedate into a time object
+                    DateTime linedate = new DateTime(
+                                        currdate.Year,  // Jahr fake it to the current year... common bliss was it too much to include it too?
+                                        Convert.ToInt32(group_match["months"].Value), // month
+                                        Convert.ToInt32(group_match["days"].Value), // day
+                                        Convert.ToInt32(group_match["hours"].Value), // hour
+                                        Convert.ToInt32(group_match["minutes"].Value), // minute
+                                        Convert.ToInt32(group_match["seconds"].Value) // second
+                                        );
+
+                    // get the difference between the last and the current line
+                    timediff = linedate - lastdate;
+
+                    // apply the date of the last line
+                    lastdate = linedate;
+
+                    // if the timeframe between 2 lines is greater than 2 hours or
+                    // we are at the first line, create a new logfile
+                    if (Convert.ToInt32(timediff.TotalSeconds) > Convert.ToInt32(edt_splittime.Text) || line_count == 0)
+                    {
+
+                        logname = "WoWCombatLog_" + String.Format("{0:" + edt_dateformat.Text + "}", lastdate) + ".txt";
+
+                        newlogname = path + "\\" + logname;
+
+
+                        lbl_combatlog.Text = logname;
+                        diff_logs++;
+
+                        if (oldlogname != newlogname && !string.IsNullOrEmpty(oldlogname))
+                        {
+                            sw_nlog.Close();
+                            fs_nlog.Close();
+
+                            // here comes some ziping algorithm later i think O.o!! (Todo)
+
+                        }
+
+                        // try to open a new file to write in!
+                        // if the file exists delete it and create a new one!
+                        try
+                        {
+                            if (File.Exists(newlogname))
+                            {
+                                try
+                                {
+                                    File.Delete(newlogname);
+                                }
+                                catch (Exception ex)
+                                {
+                                    lbl_combatlog.Text = "could not delete file " + Path.GetFileName(newlogname);
+                                    // Progressbar resetten
+                                    prg_parse.Value = 0;
+                                    parsetime.Stop();
+                                    return;
+                                }
+                            }
+                            fs_nlog = new FileStream(newlogname, FileMode.CreateNew, FileAccess.Write);
+                            sw_nlog = new StreamWriter(fs_nlog);
+                        }
+                        catch (Exception ex)
+                        {
+                            // throw an error and provide the filename where the error occured
+                            lbl_combatlog.Text = "Cannot open " + Path.GetFileName(newlogname);
+                            // Progressbar resetten
+                            prg_parse.Value = 0;
+                            parsetime.Stop();
+                            return;
+                        }
+
+                        // safe current logname to equate it later
+                        oldlogname = newlogname;
+                    }
+
+                    // write current line to the log
+                    sw_nlog.WriteLine(line);
+
+                    line_count++;
+
+                    // fill the progressbar
+                    prg_parse.Value = (int)sr_olog.BaseStream.Position;
+
+                    // refresh application form
+                    Application.DoEvents();
+                } // while
+
+                // close file hadlers
+                // Read
+                sr_olog.Close();
+                fs_olog.Close();
+                // Read
+                sw_nlog.Close();
+                fs_nlog.Close();
+
+
+                // resetten the progressbar since we finished
+                prg_parse.Value = 0;
+
+                parsetime.Stop();
+
+                // Get the elapsed time as a TimeSpan value.
+                TimeSpan ts = parsetime.Elapsed;
+
+                lbl_combatlog.Text = String.Format("Parse time: {0:00}:{1:00}.{2:00} | logs: {3} | lines : {4}",
+                                                    ts.Hours,
+                                                    ts.Minutes,
+                                                    ts.Seconds,
+                                                    diff_logs,
+                                                    line_count
+                                                    );
+
+                // hide buttons/progressbar
+                prg_parse.Visible = false;
+                btn_abort.Visible = false;
+            }
+            else
+            {
+                if(!md.Success)
+                    MessageBox.Show("Date time format is not correct. Please try again!");
+                if(!st.Success)
+                    MessageBox.Show("Split time Format is not correct. Please try again!");
+            }
+        }
+
+        private void dlg_combatlog_FileOk(object sender, CancelEventArgs e)
+        {
+            btn_parse.Visible = true;
+            btn_parse.Enabled = true;
+            abort = false; // reset the abort var mabe it has been abortet before?
+            lbl_combatlog.Text = dlg_combatlog.FileName;
+        }
+
+        private void btn_abort_Click(object sender, EventArgs e)
+        {
+            // set abort flag
+            abort = true;
+            
+            // hide & deactivate abort button
+            btn_abort.Visible = false;
+            btn_abort.Enabled = false;
+            
+            // show parse button but deactivate it for now (the log has to be needs to be loaded again!)
+            btn_parse.Visible = true;
+            btn_parse.Enabled = false;
+
+            // activate combatlog button
+            btn_combatlog.Enabled = true;
+        }
+
+        private void lbl_link_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            lbl_link.Links[lbl_link.Links.IndexOf(e.Link)].Visited = true;
+            System.Diagnostics.Process.Start(lbl_link.Text);
+        }
+
+    }
+}
